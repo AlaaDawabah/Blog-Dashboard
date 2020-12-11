240b109cdff44adfb452c6cfbe05d443bb26041c
alaaahmeddawabah <alaa.ahmed@ibtikar.net.sa>
2020-12-12 00:28:49 +0200

    add style to cards

---
+++ src/pages/Blogs/Blogs.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getBlogsList } from "../../store/actions/Blog";
+import Blog from "../../components/Blog";
+import history from "../../routes/History";
+import "./Blogs.css";
+const Blogs = () => {
+  const dispatch = useDispatch();
+  const {
+    blogs: { blogsList }
+  } = useSelector(state => state);
+  useEffect(() => {
+    dispatch(getBlogsList());
+  }, []);
+  return (
+    <>
+      <div className="row justify-content-between">
+        <div className="col-md-6">
+          <button
+            onClick={() => history.push(`/add-blog`)}
+            className="add_blog_btn mb-4"
+          >
+            <span>
+              <svg
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 16 16"
+                class="bi bi-plus"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                className="pr-1"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+                />
+              </svg>
+              Add Blog
+            </span>
+          </button>
+        </div>
+        <div className="col-md-6 d-flex justify-content-end">
+          <input type="text" className="px-3 search" placeholder={`Search`} />
+        </div>
+      </div>
+      <div>
+        {blogsList && blogsList.map((blog, i) => <Blog blog={blog} key={i} />)}
+      </div>
+    </>
+  );
+};
+
+export default Blogs;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,36 +1,14 @@
-import React, { useEffect } from "react";
-import {
-  Switch,
-  Route,
-  Router,
-  BrowserRouter,
-  useHistory,
-  useLocation
-} from "react-router-dom";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import AddBlog from "../components/AddBlog";
-import Blogs from "../pages/Blogs";
-import history from "./History"
+import Blogs from "../pages/Blogs/Blogs";
 const Routes = () => {
-  const location = useLocation();
-  const History = useHistory();
-  // useEffect(() => {
-  //   History.push({
-  //     search: location.search,
-  //     state: location.state
-  //   });
-  // }, []);
   return (
-    // <div>
-    // <BrowserRouter>
-      // <Router history={history}>
-      <Switch>
-        <Route path={`/`} component={Blogs} exact />
-        <Route path={`/add-blog`} component={AddBlog} exact />
-        <Route path={`/edit-blog`} component={AddBlog} exact />
-      </Switch>
-      // </Router>
-    // </BrowserRouter>
-    // </div>
+    <Switch>
+      <Route path={`/`} component={Blogs} exact />
+      <Route path={`/add-blog`} component={AddBlog} exact />
+      <Route path={`/edit-blog`} component={AddBlog} exact />
+    </Switch>
   );
 };
 
